45b0b9a71d95cb0e08777c2b668f1bfedcce71f9
Justin Wehrman <jwehrman@wehr.haus>
2016-10-23 18:18:47 -0500

    Added quick demo of keyboard LED control

--- src/examples/blinkLed.js
+++ src/examples/blinkLed.js
@@ -4,6 +4,8 @@ require(appRoot + '/src/utils/particleEnv');
 const five = require('johnny-five');
 const Particle = require('particle-io');
 
+let led = {};
+
 const board = new five.Board({
     io: new Particle({
         token: process.env.PARTICLE_ACCESS_TOKEN,
@@ -12,8 +14,49 @@ const board = new five.Board({
 });
 
 const led_blink = () => {
-    const led = new five.Led('D7');
+    led.off();
     led.strobe(500);
 };
 
-board.on('ready', led_blink);
+const led_on = () => {
+    led.on();
+};
+
+const led_off = () => {
+    led.stop();
+    led.off();
+};
+
+const board_ready = () => {
+    console.log('Device Ready...');
+
+    led = new five.Led('D7');
+
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.setRawMode(true);
+
+    process.stdin.on('keypress', function(ch, key) {
+        if (!key) return;
+        switch (key.name) {
+            case 'up':
+                console.log('up');
+                led_on();
+                break;
+            case 'down':
+                console.log('down');
+                led_off();
+                break;
+            case 'left':
+                console.log('left');
+                led_blink();
+                break;
+            default:
+                console.log('default');
+                led_off();
+        }
+
+    });
+};
+
+board.on('ready', board_ready);
